427ad0758fb707153f9e01f1f2aa3fee01c9d2ad
AnikiFan <2254298@tongji.edu.cn>
2024-09-11 22:00:33 +0800

    完成，并通过基本的测试

--- MyBlock/MyCrop.py
+++ MyBlock/MyCrop.py
@@ -1,54 +1,158 @@
 import os.path
-from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+import warnings
 import torch.nn as nn
 import torch
 import torchvision.transforms as T
 from torchvision.io import read_image
 import torchvision.transforms.functional as TF
-import random
 
-plt.rcParams["savefig.bbox"] = 'tight'
-torch.manual_seed(1)
+
+def show(imgs):
+    ncols, nrows = (len(imgs) + 1) // 2, 2
+    fix, axs = plt.subplots(ncols=ncols, nrows=nrows, squeeze=False)
+    for i, img in enumerate(imgs):
+        img = T.ToPILImage()(img.to('cpu'))
+        axs[i // ncols, i % ncols].imshow(np.asarray(img))
+        axs[i // ncols, i % ncols].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    for i in range(len(imgs), nrows * ncols):
+        axs[i // ncols, i % ncols].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[], )
+        for spines in axs[i // ncols, i % ncols].spines.values():
+            spines.set_visible(False)
+    plt.show()
+
 
 class MyRotationTransform(nn.Module):
     """Rotate by one of the given angles."""
 
-    def __init__(self, angles):
+    def __init__(self):
         super().__init__()
-        self.angles = angles
 
     def __call__(self, x):
-        angle = random.choice(self.angles)
-        return TF.rotate(x, angle)
+        """
+        :param x:
+        :return: 裁剪去界面后的图像
+        裁剪某一行/列的需满足以下条件之一：
+        1. 黑色比例超过阈值(黑色这里定义为三通道相同，且都小于等于20)
+        2. 白色比例超过阈值(白色这里定义为三通道相同，且都大于等于250)
+        3. 彩色比例超过阈值(彩色这里定义为三通道与平均值之差的绝对值与三通道平均值之比之和大于0.5)
+        """
+        c, h, w = x.shape
+        row_tolerance = h * 0.05
+        col_tolerance = w * 0.05
+        max_row_tolerance_time = 2
+        max_col_tolerance_time = 2
+        channel_mean = torch.mean(x.to(torch.float), dim=0)
+        channel_diff_abs = torch.abs(x - channel_mean).to(torch.uint8)
+        channel_is_same = (channel_diff_abs == 0)
+        channel_is_black = (x <= 20)
+        channel_is_white = (x >= 250)
+        channel_is_color = torch.sum(channel_diff_abs, dim=0)/channel_mean > 0.5
+        tmp = channel_is_same & (channel_is_white | channel_is_black)
+        pure_mask = tmp[0]
+        for i in range(1, tmp.size(0)):
+            pure_mask = pure_mask | tmp[i]
+
+        pure_row_mask = (torch.sum(pure_mask, dim=1) / w) >= 0.85
+        pure_col_mask = (torch.sum(pure_mask, dim=0) / h) >= 0.80
+        color_row_mask = (torch.sum(channel_is_color, dim=1) / w) >= 0.5
+        color_col_mask = (torch.sum(channel_is_color, dim=0) / h) >= 0.3
+        row_mask = pure_row_mask | color_row_mask
+        col_mask = pure_col_mask | color_col_mask
+
+        left, right, top, bottom = 0, w - 1, 0, h - 1
+
+        tolerance_cnt = 0
+        tolerance_time = 0
+        p = left
+        while p < right and tolerance_time < col_tolerance:
+            if col_mask[p]:
+                left = p
+                p += 1
+                if tolerance_cnt:
+                    tolerance_cnt = 0
+                    tolerance_time += 1
+            else:
+                if tolerance_cnt >= col_tolerance:
+                    break
+                tolerance_cnt += 1
+                p += 1
+
+        tolerance_cnt = 0
+        tolerance_time = 0
+        p = right
+        while p > left and tolerance_time < col_tolerance:
+            if col_mask[p]:
+                right = p
+                p -= 1
+                if tolerance_cnt:
+                    tolerance_cnt = 0
+                    tolerance_time += 1
+            else:
+                if tolerance_cnt >= col_tolerance:
+                    break
+                tolerance_cnt += 1
+                p -= 1
+
+        tolerance_cnt = 0
+        tolerance_time = 0
+        p = top
+        while p < bottom and tolerance_time < row_tolerance:
+            if row_mask[p]:
+                top = p
+                p += 1
+                if tolerance_cnt:
+                    tolerance_cnt = 0
+                    tolerance_time += 1
+            else:
+                if tolerance_cnt >= row_tolerance:
+                    break
+                tolerance_cnt += 1
+                p += 1
+
+        tolerance_cnt = 0
+        tolerance_time = 0
+        p = bottom
+        while p > top and tolerance_time < row_tolerance:
+            if row_mask[p]:
+                bottom = p
+                p -= 1
+                if tolerance_cnt:
+                    tolerance_cnt = 0
+                    tolerance_time += 1
+            else:
+                if tolerance_cnt >= row_tolerance:
+                    break
+                tolerance_cnt += 1
+                p -= 1
+
+        if right - left < w * 0.4:
+            warnings.warn('width too small after cropped!')
+            left, right = 0, w - 1
+        if bottom - top < h * 0.4:
+            warnings.warn('height too small after cropped!')
+            top, bottom = 0, h - 1
+
+        # DEBUG用
+        # show([x,TF.crop(x, top, left, bottom - top, right - left)])
+        return TF.crop(x, top, left, bottom - top, right - left)
 
 
-def show(imgs):
-    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
-        img = T.ToPILImage()(img.to('cpu'))
-        axs[0, i].imshow(np.asarray(img))
-        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    plt.show()
 
 
-test1 = read_image(str(os.path.join(os.pardir, 'data', 'test', '0086.jpg')))
-test2 = read_image(str(os.path.join(os.pardir, 'data', 'test', '0088.jpg')))
-show([test1, test2])
+if __name__ == '__main__':
+    plt.rcParams["savefig.bbox"] = 'tight'
+    torch.manual_seed(1)
 
-transforms = torch.nn.Sequential(
-    T.RandomCrop(224),
-    T.RandomHorizontalFlip(p=0.3),
-    MyRotationTransform(angles=[-30, -15, 0, 15, 30]),
-)
+    testImages = [read_image(os.path.join(os.pardir, 'data', 'test', image)).to(device) for image in
+                  os.listdir(os.path.join(os.pardir, 'data', 'test'))]
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-test1 = test1.to(device)
-test2 = test2.to(device)
-
-transformed_test1 = transforms(test1)
-transformed_test2 = transforms(test2)
-show([transformed_test1, transformed_test2])
+    transforms = torch.nn.Sequential(
+        MyRotationTransform(),
+        # T.RandomCrop(224),
+        # T.RandomHorizontalFlip(p=0.3),
+    )
+    for image in testImages:
+        transforms(image)

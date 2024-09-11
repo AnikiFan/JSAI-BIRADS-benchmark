a716b5ca8099eb4bb35d54d02478cf3a712b9d6d
AnikiFan <2254298@tongji.edu.cn>
2024-09-11 22:59:31 +0800

    重新整理了文件夹
    1. 整理图片的函数整合成了一个类DataOrganizer，删除了dataReorg，前者与后者等价
    2. 将神经网络中的自定义块整理到util文件夹下
    3. 更新了环境配置

--- train.py
+++ train.py
@@ -4,36 +4,35 @@ import torchvision.transforms as transforms
 from torcheval.metrics.functional import multiclass_precision, multiclass_f1_score
 import tqdm
 import os
-import os
 import json
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 # from TDSNet.TDSNet import TDSNet
 from TDSNet.TDSNet import TDSNet
-from model4compare.GoogleNet import GoogleNet
-from model4compare.AlexNet import AlexNet
-from model4compare.VGG import VGG
-from model4compare.NiN import NiN
-import logging
+from models.model4compare.GoogleNet import GoogleNet
+from models.model4compare.AlexNet import AlexNet
+from models.model4compare.VGG import VGG
+from models.model4compare.NiN import NiN
 
 from models.UnetClassifer.unet import UnetClassifier
-from utils.datasetCheck import checkDataset # 数据集检查
-from utils.earlyStopping import EarlyStopping # 提前停止
-from utils.multiMessageFilter import setup_custom_logger  #! 把MultiMessageFilter放入/utils.multiMessageFilter.py文件中
-logger = setup_custom_logger() # 设置日志屏蔽器，屏蔽f1_score的warning
-from utils.tools import getDevice, create_transforms # getDevice获取设备，create_transforms根据json配置创建transforms对象
+from utils.datasetCheck import checkDataset  # 数据集检查
+from utils.earlyStopping import EarlyStopping  # 提前停止
+from utils.multiMessageFilter import setup_custom_logger  # ! 把MultiMessageFilter放入/utils.multiMessageFilter.py文件中
 
+from utils.tools import getDevice, create_transforms  # getDevice获取设备，create_transforms根据json配置创建transforms对象
+
+logger = setup_custom_logger()  # 设置日志屏蔽器，屏蔽f1_score的warning
 
 # 配置
 cfg = {
-    "model": "Unet", # 模型选择
-    "data": "Breast", # 数据集选择
-    "epoch_num": 1000, # 训练的 epoch 数量
-    "batch_size": 4, # 批处理大小
-    "in_channels": 3, # 输入通道数（图像）
-    "device": getDevice(), # 设备，自动检测无需修改
-    "pin_memory": True if getDevice() == "cuda" else False, # 是否使用 pin_memory，无需修改
-    "infoShowFrequency": 100 # 信息显示频率(每多少个 batch 输出一次信息)
+    "model": "Unet",  # 模型选择
+    "data": "Breast",  # 数据集选择
+    "epoch_num": 1000,  # 训练的 epoch 数量
+    "batch_size": 4,  # 批处理大小
+    "in_channels": 3,  # 输入通道数（图像）
+    "device": getDevice(),  # 设备，自动检测无需修改
+    "pin_memory": True if getDevice() == "cuda" else False,  # 是否使用 pin_memory，无需修改
+    "infoShowFrequency": 100  # 信息显示频率(每多少个 batch 输出一次信息)
 }
 
 # 模型配置，不同模型配置不同
@@ -76,7 +75,6 @@ model_cfg = {
     }
 }
 
-
 # transform_train = torchvision.transforms.Compose([
 #     torchvision.transforms.Resize((40, 40)),  # 缩放到 40x40 像素
 #     # torchvision.transforms.RandomResizedCrop(32, scale=(0.64, 1.0), ratio=(1.0, 1.0)),  # 随机裁剪并缩放到 32x32
@@ -115,8 +113,7 @@ transforms_cfg = {
 }
 
 
-
-def train_one_epoch(model,train_loader,epoch_index,num_class, tb_writer):
+def train_one_epoch(model, train_loader, epoch_index, num_class, tb_writer):
     '''
     训练一个 epoch
     :param model: 模型
@@ -130,10 +127,10 @@ def train_one_epoch(model,train_loader,epoch_index,num_class, tb_writer):
     running_f1 = 0.
     last_loss = 0.
     last_precision = 0.
-    last_f1 = 0.    
+    last_f1 = 0.
 
     model.train(True)
-    
+
     # Here, we use enumerate(training_loader) instead of
     # iter(training_loader) so that we can track the batch
     # index and do some intra-epoch reporting
@@ -164,7 +161,7 @@ def train_one_epoch(model,train_loader,epoch_index,num_class, tb_writer):
         # Gather data and report
         running_loss += loss.item()
         frequency = cfg["infoShowFrequency"]
-        if i % frequency == frequency-1:
+        if i % frequency == frequency - 1:
             last_loss = running_loss / frequency  # loss per batch
             last_precision = running_precision / frequency  # loss per batch
             last_f1 = running_f1 / frequency  # loss per batch
@@ -204,11 +201,12 @@ def modelSelector(model, lr, num_class):
     elif model == 'NiN':
         model = NiN(num_class)
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-        return model,SummaryWriter('runs/NiN_'+str(lr)+"_"+timestamp),optimizer,timestamp
+        return model, SummaryWriter('runs/NiN_' + str(lr) + "_" + timestamp), optimizer, timestamp
     elif model == 'Unet':
-        model = UnetClassifier(num_classes=num_class,in_channels = cfg['in_channels'], backbone=model_cfg[model]["backbone"],pretrained=model_cfg[model]["pretrained"])
+        model = UnetClassifier(num_classes=num_class, in_channels=cfg['in_channels'],
+                               backbone=model_cfg[model]["backbone"], pretrained=model_cfg[model]["pretrained"])
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-        return model,SummaryWriter('runs/Unet_'+str(lr)+"_"+timestamp),optimizer,timestamp
+        return model, SummaryWriter('runs/Unet_' + str(lr) + "_" + timestamp), optimizer, timestamp
 
 
 def dataSelector(data='Breast'):
@@ -224,7 +222,7 @@ def dataSelector(data='Breast'):
         transform_test = create_transforms(transforms_cfg["transform_test"])
         print("transform_train: ", transform_train)
         print("transform_test: ", transform_test)
-        
+
         # 创建数据集
         train_ds, train_valid_ds = [
             torchvision.datasets.ImageFolder(
@@ -249,28 +247,30 @@ def dataSelector(data='Breast'):
 if __name__ == '__main__':
     "----------------------------------- data ---------------------------------------------"
     print("-------------------------- preparing data... --------------------------")
-    train_ds, valid_ds = dataSelector('Breast') # 数据集选择
-    num_class = len(train_ds.classes) # 获取类别数量
-    
+    train_ds, valid_ds = dataSelector('Breast')  # 数据集选择
+    num_class = len(train_ds.classes)  # 获取类别数量
+
     # 创建数据加载器    
-    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=cfg["batch_size"], shuffle=True, pin_memory=cfg["pin_memory"], drop_last=True)
-    valid_loader = torch.utils.data.DataLoader(valid_ds, batch_size=cfg["batch_size"], shuffle=False, pin_memory=cfg["pin_memory"], drop_last=True)
+    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=cfg["batch_size"], shuffle=True,
+                                               pin_memory=cfg["pin_memory"], drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(valid_ds, batch_size=cfg["batch_size"], shuffle=False,
+                                               pin_memory=cfg["pin_memory"], drop_last=True)
     # 检查数据集，输出相关信息
-    checkDataset(train_ds, valid_ds, train_loader, valid_loader,num_samples_to_show=0) # 
+    checkDataset(train_ds, valid_ds, train_loader, valid_loader, num_samples_to_show=0)  #
     "----------------------------------- loss function ---------------------------------------------"
     # if model_cfg[cfg["model"]]["loss_fn"] == "CrossEntropyLoss":
     loss_fn = torch.nn.CrossEntropyLoss()
 
     "----------------------------------- model ---------------------------------------------"
     print("-------------------------- preparing model... --------------------------")
-    model,writer,optimizer,timestamp = modelSelector(cfg["model"], model_cfg[cfg["model"]]["lr"], num_class)
+    model, writer, optimizer, timestamp = modelSelector(cfg["model"], model_cfg[cfg["model"]]["lr"], num_class)
     model.to(torch.device(cfg["device"]))
 
     "----------------------------------- training ---------------------------------------------"
     epoch_number = 0
 
     # EPOCHS = 5
-    
+
     # 实例化 EarlyStopping，设定耐心值和最小变化
     early_stopping = EarlyStopping(patience=10, min_delta=0.01)
 
@@ -302,7 +302,8 @@ if __name__ == '__main__':
                 voutputs = model(vinputs)
                 vloss = loss_fn(voutputs, vlabels)
                 vprecision = multiclass_precision(voutputs, vlabels).tolist()
-                vf1 = multiclass_f1_score(voutputs, vlabels, average='macro', num_classes=num_class).tolist() #note:设置成macro进而计算每个类别的f1值
+                vf1 = multiclass_f1_score(voutputs, vlabels, average='macro',
+                                          num_classes=num_class).tolist()  # note:设置成macro进而计算每个类别的f1值
                 running_vloss += vloss
                 running_vprecison += vprecision
                 running_vf1 += vf1
@@ -332,22 +333,22 @@ if __name__ == '__main__':
         if early_stopping.early_stop:
             print("Early stopping triggered!")
             break
-        
+
         # Track best performance, and save the model's state
         if avg_vloss < best_vloss:
             print(f"Validation loss improved from {best_vloss:.6f} to {avg_vloss:.6f} - saving model")
             best_vloss = avg_vloss
-            
+
             # if not os.path.exists('checkPoint'):
             #     os.makedirs('checkPoint')
             # if not os.path.exists(f'checkPoint/{cfg["model"]}_{cfg["data"]}_{timestamp}'):
             #     os.makedirs(f'checkPoint/{cfg["model"]}_{timestamp}')
-            
-            #e.g checkPoint/Unet_Breast_20210929_123456
+
+            # e.g checkPoint/Unet_Breast_20210929_123456
             checkPoint_path = os.path.join(os.getcwd(), 'checkPoint', f'{cfg["model"]}_{cfg["data"]}_{timestamp}')
             if not os.path.exists(checkPoint_path):
                 os.makedirs(checkPoint_path)
-            
+
             # 将cfg和model_cfg保存到json文件
             # e.g checkPoint/Unet_Breast_20210929_123456/cfg.json
             with open(os.path.join(checkPoint_path, 'cfg.json'), 'w') as f:
@@ -356,17 +357,19 @@ if __name__ == '__main__':
                 json.dump(model_cfg, f)
             with open(os.path.join(checkPoint_path, 'transforms_cfg.json'), 'w') as f:
                 json.dump(transforms_cfg, f)
-            
+
             modelCheckPoint_path = os.path.join(checkPoint_path, 'model')
             if not os.path.exists(modelCheckPoint_path):
                 os.makedirs(modelCheckPoint_path)
-            torch.save(model.state_dict(), modelCheckPoint_path+f'/{cfg["model"]}_ac{avg_vprecision:.6f}_f1{avg_vf1:.6f}_{epoch}.pth')
+            torch.save(model.state_dict(),
+                       modelCheckPoint_path + f'/{cfg["model"]}_ac{avg_vprecision:.6f}_f1{avg_vf1:.6f}_{epoch}.pth')
             optimizerCheckPoint_path = os.path.join(checkPoint_path, 'optimizer')
             if not os.path.exists(optimizerCheckPoint_path):
                 os.makedirs(optimizerCheckPoint_path)
-            torch.save(optimizer.state_dict(), optimizerCheckPoint_path+f'/{cfg["model"]}_ac{avg_vprecision:.6f}_f1{avg_vf1:.6f}_{epoch}.pth')
+            torch.save(optimizer.state_dict(),
+                       optimizerCheckPoint_path + f'/{cfg["model"]}_ac{avg_vprecision:.6f}_f1{avg_vf1:.6f}_{epoch}.pth')
 
         epoch_number += 1
-        
+
     print("-------------------------- training finished --------------------------")
     print(f'time: {datetime.now()}')

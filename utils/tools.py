2e423245e7f09d14db4ac11b6dadbab5f396298e
Yang Jay <129719736+YangJay2004@users.noreply.github.com>
2024-09-14 19:20:47 +0800

    更新了断点保存和断点续训机制

--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.transforms as transforms
+import os
 
 # 检查设备并设置 device
 def getDevice():
@@ -24,3 +25,28 @@ def create_transforms(transform_config):
         transform_list.append(transform)
     return transforms.Compose(transform_list)
 
+
+def save_checkpoint(state, checkpoint_dir, filename='checkpoint.pth.tar'):
+    filepath = os.path.join(checkpoint_dir, filename)
+    relative_filepath = os.path.relpath(filepath)
+    print(f"=> saving checkpoint at '{relative_filepath}'")
+    torch.save(state, filepath)
+
+def load_checkpoint(model, optimizer, checkpoint_path):
+    if os.path.isfile(checkpoint_path):
+        print(f"=> loading checkpoint '{checkpoint_path}'")
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        best_vloss = checkpoint['best_vloss']
+        print(f"=> loaded checkpoint '{checkpoint_path}' (epoch {epoch})")
+        print(f"-- current epoch: {epoch}")
+        print(f"-- current lr: {optimizer.param_groups[0]['lr']}")
+        print(f"-- current best_vloss: {best_vloss}")
+        return model, optimizer, epoch, best_vloss
+    else:
+        print(f"=> no checkpoint found at '{checkpoint_path}'")
+        return model, optimizer, 0, float('inf')
+    
+    

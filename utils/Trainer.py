ff24c5ee4457e47814ac81cabe6531c5c15f8be8
AnikiFan <2254298@tongji.edu.cn>
2024-10-12 11:44:11 +0800

    更新train_one_epoch对于整个训练集的指标计算方式
    之前其实只是使用了其中info_frequency大小的批次进行计算

--- utils/Trainer.py
+++ utils/Trainer.py
@@ -23,7 +23,7 @@ class Trainer:
         self.loss_fn = instantiate(cfg.train.loss_function)
         self.cur_fold = 1
         # 用于统计各折之间的指标
-        self.loss, self.f1_score, self.accuracy, self.confusion_matrix = None,None,None,None
+        self.loss, self.f1_score, self.accuracy, self.confusion_matrix = None, None, None, None
         self.train_transform = instantiate(self.cfg.train_transform)
         self.valid_transform = instantiate(self.cfg.valid_transform)
         # 用于记录以折为单位的最佳指标
@@ -44,7 +44,7 @@ class Trainer:
                            pin_memory_device=self.cfg.env.pin_memory_device)
             )
             if self.loss is None:
-                self.loss,self.f1_score,self.accuracy,self.confusion_matrix = loss,f1_score,accuracy,confusion_matrix
+                self.loss, self.f1_score, self.accuracy, self.confusion_matrix = loss, f1_score, accuracy, confusion_matrix
             else:
                 self.loss += loss
                 self.f1_score += f1_score
@@ -61,7 +61,8 @@ class Trainer:
             info(f'ACCURACY             :{self.accuracy:.10f}')
             info(f'F1                   :{self.f1_score:.10f}')
             info(f'confusion matrix:\n{str(self.confusion_matrix)}')
-        return instantiate(self.cfg.train.choose_strategy,loss=self.loss,accuracy=self.accuracy,f1_score=self.f1_score)
+        return instantiate(self.cfg.train.choose_strategy, loss=self.loss, accuracy=self.accuracy,
+                           f1_score=self.f1_score)
 
     def train_one_epoch(self, *, model, train_loader: DataLoader, optimizer, epoch_index: int,
                         tb_writer: SummaryWriter) -> Tuple[float, float, float]:
@@ -74,6 +75,7 @@ class Trainer:
         :param tb_writer: TensorBoard 写入器
         '''
         outputs, labels = [], []
+        train_outputs, train_labels = [], []
 
         model.train(True)
 
@@ -91,19 +93,25 @@ class Trainer:
             if (i % self.cfg.train.info_frequency == 0
                     or len(train_loader) < self.cfg.train.info_frequency and i == len(train_loader) - 1):
                 # 计算实际的批次数
+                train_outputs.extend(outputs)
+                train_labels.extend(labels)
                 outputs, labels = torch.cat(outputs, dim=0), torch.cat(labels, dim=0)
                 avg_loss = self.loss_fn(input=outputs, target=labels).item()
-                avg_f1 = instantiate(self.cfg.train.f1_score, input=outputs, target=labels,
-                                     num_classes=self.cfg.dataset.num_classes).item()
                 avg_accuracy = instantiate(self.cfg.train.accuracy, input=outputs, target=labels,
                                            num_classes=self.cfg.dataset.num_classes).item()
+                avg_f1 = instantiate(self.cfg.train.f1_score, input=outputs, target=labels,
+                                     num_classes=self.cfg.dataset.num_classes).item()
                 tb_x = (epoch_index - 1) * len(train_loader) + i
                 tb_writer.add_scalar('Loss/train', avg_loss, tb_x)
                 tb_writer.add_scalar('Accuracy/train', avg_accuracy, tb_x)
                 tb_writer.add_scalar('F1/train', avg_f1, tb_x)
                 outputs, labels = [], []
         # 为了避免指标出现大幅波动，不对尾部剩余的一小部分计算指标
-        return avg_loss, avg_accuracy, avg_f1
+        train_outputs = torch.cat(train_outputs, dim=0)
+        train_labels = torch.cat(train_labels, dim=0)
+        return (self.loss(input=train_outputs, label=train_labels).item(),
+                instantiate(self.cfg.train.accuracy,input=train_outputs, target=train_labels,num_classes=self.cfg.dataset.num_classes).item(),
+                instantiate(self.cfg.train.f1_score, input=train_outputs, target=train_labels, num_classes=self.cfg.dataset.num_classes).item())
 
     def make_writer_title(self) -> str:
         """
@@ -146,13 +154,13 @@ class Trainer:
         for epoch in range(1, self.cfg.train.epoch_num + 1):
             # 训练一个epoch，获取在上面的指标
             avg_loss, avg_accuracy, avg_f1 = self.train_one_epoch(model=model, train_loader=train_loader,
-                                                                optimizer=optimizer, epoch_index=epoch,
-                                                                tb_writer=writer)
+                                                                  optimizer=optimizer, epoch_index=epoch,
+                                                                  tb_writer=writer)
             schedular.step()
             model.eval()
             with torch.no_grad():
                 valid_outcomes = [(vlabel.to(self.cfg.env.device), model(vinputs.to(self.cfg.env.device))) for
-                                vinputs, vlabel in tqdm(valid_loader, desc=f"Validating Epoch {epoch}", leave=False)]
+                                  vinputs, vlabel in tqdm(valid_loader, desc=f"Validating Epoch {epoch}", leave=False)]
             target = torch.cat([pair[0] for pair in valid_outcomes], dim=0)
             prediction = torch.cat([pair[1] for pair in valid_outcomes], dim=0)
             # 在该epoch的验证集上获得的指标
@@ -160,9 +168,9 @@ class Trainer:
             avg_vaccuracy = instantiate(self.cfg.train.accuracy, input=prediction.cpu(), target=target.cpu(),
                                         num_classes=self.cfg.dataset.num_classes).item()
             avg_vf1 = instantiate(self.cfg.train.f1_score, input=prediction.cpu(), target=target.cpu(), average='macro',
-                                num_classes=self.cfg.dataset.num_classes).item()
+                                  num_classes=self.cfg.dataset.num_classes).item()
             confusion_matrix = instantiate(self.cfg.train.confusion_matrix, input=prediction.cpu(), target=target.cpu(),
-                                        num_classes=self.cfg.dataset.num_classes)
+                                           num_classes=self.cfg.dataset.num_classes)
             info(f"----------------- Epoch {epoch} Summary -----------------")
             info('LOSS      train {:.10f} valid {:.10f}'.format(avg_loss, avg_vloss))
             info('ACCURACY  train {:.10f} valid {:.10f}'.format(avg_accuracy, avg_vaccuracy))
@@ -171,12 +179,12 @@ class Trainer:
             # Log the running loss averaged per batch
             # for both training and validation
             writer.add_scalars('Training vs. Validation Loss', {'Training': avg_loss, 'Validation': avg_vloss},
-                            epoch)
+                               epoch)
             writer.add_scalars('Training vs. Validation Accuracy',
-                            {'Training': avg_accuracy, 'Validation': avg_vaccuracy},
-                            epoch)
+                               {'Training': avg_accuracy, 'Validation': avg_vaccuracy},
+                               epoch)
             writer.add_scalars('Training vs. Validation F1', {'Training': avg_f1, 'Validation': avg_vf1},
-                            epoch)
+                               epoch)
             writer.add_text(f'confusion matrix of epoch {epoch}', str(confusion_matrix))
             writer.flush()
 
@@ -187,8 +195,8 @@ class Trainer:
             if avg_vloss < self.best_vloss:
                 self.best_vloss, self.best_vf1, self.best_vaccuracy, self.best_vconfusion_matrix = avg_vloss, avg_vf1, avg_vaccuracy, confusion_matrix
                 info(f"\n############################################################\n"
-                    f"# Validation loss improved to {avg_vloss:.6f} - saving best model #\n"
-                    f"############################################################")
+                     f"# Validation loss improved to {avg_vloss:.6f} - saving best model #\n"
+                     f"############################################################")
                 # 保存checkpoint（包括epoch，model_state_dict，optimizer_state_dict，best_vloss，但仅在best时保存）
                 # 保存断点重训所需的信息（需要包括epoch，model_state_dict，optimizer_state_dict，best_vloss）
                 # 只保留一份最佳指标对应的参数

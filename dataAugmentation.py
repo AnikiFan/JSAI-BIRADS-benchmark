6779c5828ed6147ad7232f2e2651e09bba40eccb
YangJay2004 <zhenghuiyang0058@gmail.com>
2024-10-16 06:59:00 +0000

    chore:根目录下进行了一些数据增强，不改变框架功能

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -22,15 +22,15 @@ import numpy as np
 '''
 
 
-    
 
 if __name__ == '__main__':
     # initial_num = np.array([849, 1061, 404, 274, 232, 269])
     initial_num = np.array([1035, 1349, 492, 341, 301, 343])
-    target_num = np.array([500, 500, 500, 500, 500, 500])
+    target_num = np.array([3000-1035, 3000-1349, 3000-492, 3000-341, 3000-301, 3000-343])
     print("target_num: ", target_num)
     ratio = (target_num) / initial_num
     ratio = ratio.tolist()
+    print("ratio: ", ratio)
 
     # 设置要使用的增广策略
     selected_transforms = [
@@ -39,31 +39,48 @@ if __name__ == '__main__':
         # A.VerticalFlip(p=1.0),
         # A.RandomBrightnessContrast(brightness_limit=
         # 0.2, contrast_limit=0.2, p=1.0),
+        
+        A.Compose([
+            A.GaussNoise(var_limit=(10.0, 50.0), p=1.0),
+        ]),
+        
         # A.GaussNoise(var_limit=(10.0, 50.0), p=1.0),
-        # A.ElasticTransform(alpha=1.0, sigma=50, p=1.0),
+        A.Compose([
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
+        ]),
+        
+        A.Compose([
+            A.ElasticTransform(alpha=1.0, sigma=50, alpha_affine=50, p=1.0),
+        ]),
+        
         # A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=1.0),
         # A.GaussianBlur(blur_limit=(3, 7), p=1.0),
         # A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=10, p=1.0)
-        A.Rotate(limit=10, p=1.0),
+        
+        A.Compose([
+            A.Rotate(limit=10, p=1.0)
+        ]),
         # A.HorizontalFlip(p=1.0), 
         # A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
         # A.GaussNoise(var_limit=(10.0, 50.0), p=1.0),
         # A.ElasticTransform(alpha=1.0, sigma=50, alpha_affine=50, p=1.0),
         # A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=0, p=1.0),
         # A.RandomGamma(gamma_limit=(80, 120), p=1.0)
-        # A.Compose([
-        #     A.Rotate(limit=10, p=0.5),
-        #     A.HorizontalFlip(p=0.5), 
-        #     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
-        #     A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
-        #     A.ElasticTransform(alpha=1.0, sigma=50, alpha_affine=50, p=0.5),
-        #     A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=0, p=0.5),
-        #     A.RandomGamma(gamma_limit=(80, 120), p=0.5)
-        # ])
+        A.Compose([
+            A.Rotate(limit=10, p=0.5),
+            A.HorizontalFlip(p=0.5), 
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+            A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
+            A.ElasticTransform(alpha=1.0, sigma=50, alpha_affine=50, p=0.5),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=0, p=0.5),
+            A.RandomGamma(gamma_limit=(80, 120), p=0.5)
+        ])
     ]
     
+    preprocess = Preprocess(selected_transforms[0], ratio=ratio)
+    
     # 转化成compose
-    selected_transforms = [A.Compose([transform]) for transform in selected_transforms]
+    selected_transforms = [transform for transform in selected_transforms]
     
     
     for transform in selected_transforms:
@@ -78,7 +95,7 @@ if __name__ == '__main__':
     # 单独处理 MixUp
     mixup_transforms = [
         'mixup_0.1',
-        'mixup_0.3'
+        # 'mixup_0.3'
         # 根据需要添加或移除 MixUp 策略
     ]
     
